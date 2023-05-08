60940bc6cb3ace83381773bd4ec470a24d7b5b39
herreraten <112837135+herreraten@users.noreply.github.com>
2023-05-08 23:34:40 +0800

    Create Siargao.java

---
+++ LONGEXAM1/Locations/Siargao.java
@@ -0,0 +1,13 @@
+package Locations;
+
+import Tourists.Tourist;
+
+public class Siargao implements Locations {
+
+    public int airFare = 300;
+
+    
+    public void accept(Tourist tourist) {
+        tourist.visit(this);
+    }
+}
